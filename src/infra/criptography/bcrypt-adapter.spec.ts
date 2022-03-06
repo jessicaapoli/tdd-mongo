c8d4e64deee7fa0567173daf4ae97c89c7a8ff1b
Jéssica A <jessica.mec.ctc@gmail.com>
2022-03-06 16:34:00 -0300

    test: ensure BcryptAdapter calls bcrypt with correct values

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
@@ -8,6 +9,14 @@ jest.mock('bcrypt', () => ({
 
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on succes', async () => {
     const salt = 12
     const sut = new BcryptAdapter(salt)
     const hash = await sut.encrypt('any_value')
